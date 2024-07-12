d995951bf66cf30fa80f99ce3fe8e0150d2e53aa
Bobby Kumar <bobbykumark571@gmail.com>
2024-07-12 21:21:13 +0530

    reactlearning

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,17 @@
-
+import Chai from "../bobby"
 
 
 
 
 function App() {
-  
+  const username ="chai aur code"
 
   return (
-   <h1>chai aur react with vite | Bobby kumar </h1>
+    <>
+   <Chai />
+   <h1>chai aur react | {username}</h1>
+   <p>test para</p>
+   </>
   )
 }
 
