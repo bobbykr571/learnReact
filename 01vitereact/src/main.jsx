cb5eff4b8c9705de6e3cf8dfe4932aa35221b818
Bobby Kumar <bobbykumark571@gmail.com>
2024-09-17 23:26:23 +0530

    tailwind&props

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -19,7 +19,7 @@ const anotherUser = "chai aur react"
 const reactElement = React.createElement(
     'a',
     {href: 'https://google.com',target:'_blank' },
-    'click me to visit google',
+    'click me to visit google ',
     anotherUser
 )
 ReactDOM.createRoot(document.getElementById('root')).render(
